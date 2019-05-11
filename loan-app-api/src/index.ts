b4a9a93ab5814ce99f46c645c59b067b81215372
hudongwen <765220914@qq.com>
2019-05-11 14:18:37 +0800

    app.set is not a function.

--- loan-app-api/src/index.ts
+++ loan-app-api/src/index.ts
@@ -13,7 +13,6 @@ export async function main(options?: ApplicationConfig) {
 
   await app.boot();
   await app.start();
-  app.set('trust proxy', 'loopback');
   // app.models.Todo.definition.rawProperties.createTime.default =
   //   app.models.Todo.definition.properties.createTime.default = function () {
   //     return getCurTimestamp();
